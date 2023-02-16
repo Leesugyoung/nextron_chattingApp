1f9b4b8a36c73a007a2c9d0edec2576e9a8421fb
sugyoung lee <tnrud4685@naver.com>
2023-02-16 17:33:36 +0900

    23.02.16 - next.config.js fix

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,4 +1,19 @@
 module.exports = {
+  target: "serverless",
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-eval'; img-src 'self' data:; style-src 'self' 'unsafe-inline'; font-src 'self' data:;",
+          },
+        ],
+      },
+    ];
+  },
   trailingSlash: true,
   webpack: (config, { isServer }) => {
     if (!isServer) {
